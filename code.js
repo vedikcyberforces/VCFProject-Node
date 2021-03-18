b88f749ab4d5586fd5a99365d3a65f7d1c2d0615
Himanshu Jangid <iamahalofan@gmail.com>
2021-03-18 21:31:15 +0530

    New Commit

--- code.js
+++ code.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 
-fs.renameSync("./a.txt", "./b.txt")
-fs.rename("somem.txt");
+// fs.renameSync("./a.txt", "./b.txt")
+// fs.rename("somem.txt");
+if(fs.existsSync("b.txt")){
+    console.log("It does Exist")
+}
 //i AM GONNA WORK ON IT LATER
