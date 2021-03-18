9f3fde87c96932580c1304c07f8120dfb051113f
Himanshu Jangid <iamahalofan@gmail.com>
2021-03-18 21:31:25 +0530

    One New Commit

--- code.js
+++ code.js
@@ -1,8 +1,45 @@
 const fs = require('fs');
-
 // fs.renameSync("./a.txt", "./b.txt")
 // fs.rename("somem.txt");
 if(fs.existsSync("b.txt")){
     console.log("It does Exist")
 }
 //i AM GONNA WORK ON IT LATER
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
