a4ca67032b3c95061a2349073ecdeb5657258bfb
Himanshu Jangid <iamahalofan@gmail.com>
2021-03-27 08:31:38 +0530

    Home page

--- test.js
+++ test.js
@@ -1,8 +1,38 @@
-const { debugPort } = require("process");
-const url = require("url");
-
-var c_url = new URL("https://this.com/submit?id=12&id=13&id=15&class=English");
-var s_p = c_url.searchParams;
+const https = require("https");
 
 
-// console.log(s_p);
+
+var obj = {};
+year = 2011
+
+
+function data(year){
+    
+    for (var i = 1; i <= 13; i++) {
+        https.get("https://jsonmock.hackerrank.com/api/football_matches?competition=UEFA%20Champions%20League&year=" + year + "&page=" + i, (res) => {
+            res.on('data', (data) => {
+                var json = JSON.parse(data);
+                for (var j = 0; j < json.data.length; j++) {
+                    if(json.data[j].team1 in obj){
+                        obj[json.data[j].team1] += 1;
+                    }
+                    else{
+                       obj[json.data[j].team1] = 1;
+                    }
+                    if(json.data[j].team2 in obj){
+                        obj[json.data[j].team1] += 1;
+                    }
+                    else{
+                       obj[json.data[j].team2] = 1;
+                    }
+                    if(i == 13 && j == ){
+                        console.log(obj);
+                    }
+                }
+            })
+        })
+        
+    }
+
+
+}
